c4e293b0478f6363b876dcbd8f2fb390361a3818
Jeswin-J <jeswinjoseph164@gmail.com>
2024-04-22 16:47:50 +0530

    frontend bug fixes

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
   return (
     <footer>
         <p>Get in touch!</p>
-            <i class="bi bi-linkedin"></i>&nbsp;&nbsp;Jeswin Joseph J
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <i class="bi bi-github"></i>&nbsp;&nbsp;Jeswin-J
+            <NavLink className={"social-links"} to='https://www.linkedin.com/in/jeswinjosephj/'><i class="bi bi-linkedin"></i>&nbsp;&nbsp;Jeswin Joseph J</NavLink>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink className={"social-links"} to='https://github.com/Jeswin-J'><i class="bi bi-github"></i>&nbsp;&nbsp;Jeswin-J</NavLink>
     </footer>
   )
 }
